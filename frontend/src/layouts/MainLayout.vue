b44aa8b01e77d7e8ab2dd714c708b80e89e47e86
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-23 03:22:06 -0600

    Implementación Sidebar

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,12 +1,24 @@
 <script setup>
-import Navbar from '../components/Navbar.vue'
+import Sidebar from '../components/Sidebar.vue'
 </script>
 
 <template>
-  <div>
-    <Navbar />
-    <main style="padding: 20px">
-      <slot />
+  <div class="layout">
+    <Sidebar />
+
+    <main class="content">
+      <router-view />
     </main>
   </div>
 </template>
+
+<style scoped>
+.layout {
+  display: flex;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
+</style>
