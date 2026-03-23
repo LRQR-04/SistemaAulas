df6ef345612e8bb91ef753eb7635f86858c5c904
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-22 19:59:57 -0600

    Layout

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navbar from '../components/Navbar.vue'
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <main style="padding: 20px">
+      <slot />
+    </main>
+  </div>
+</template>
