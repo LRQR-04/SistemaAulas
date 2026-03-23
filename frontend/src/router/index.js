669eab5ca054cfdd99ec413eec958f5370bd1299
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-22 20:01:51 -0600

    Router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../store/auth";
+
+import Login from "../views/Login.vue";
+import Dashboard from "../views/Dashboard.vue";
+import Reservaciones from "../views/Reservaciones.vue";
+import Espacios from "../views/Espacios.vue";
+import Registro from "../views/Registro.vue";
+
+const rutas = [
+  { path: "/", component: Login },
+  { path: "/registro", component: Registro },
+  { path: "/dashboard", component: Dashboard },
+  { path: "/reservaciones", component: Reservaciones },
+  { path: "/espacios", component: Espacios },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  history: createWebHistory(),
+  routes: rutas,
+});
 
-export default router
+router.beforeEach((to, from) => {
+  const auth = useAuthStore();
+
+  if (to.path !== "/" && to.path !== "/registro" && !auth.token) {
+    return "/";
+  }
+
+  return true;
+});
+
+export default router;
