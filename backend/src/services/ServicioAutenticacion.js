4d69d26f47ba26c2f5184153c1f28ba3ed406b77
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-22 22:29:40 -0600

    Validaciones de datos

--- backend/src/services/ServicioAutenticacion.js
+++ backend/src/services/ServicioAutenticacion.js
@@ -6,10 +6,8 @@ export class ServicioAutenticacion {
   static async login(correo, contrasenia) {
     const usuario = await Usuario.buscarPorEmail(correo);
 
-    if (!usuario) throw new Error("Usuario no encontrado");
-
     const valida = await bcrypt.compare(contrasenia, usuario.contrasenia);
-    if (!valida) throw new Error("Contraseña incorrecta");
+    if (!valida || !usuario) throw new Error("Credenciales incorrectas");
 
     const token = jwt.sign(
       { id: usuario.id_usuario, tipo: usuario.tipo },
@@ -23,19 +21,60 @@ export class ServicioAutenticacion {
   }
 
   static async registro(data) {
-    const rolesPermitidos = ["estudiante", "profesor"];
+    const { nombre, correo, contrasenia, tipo } = data;
 
-    if (!rolesPermitidos.includes(data.tipo)) {
-      throw new Error("Rol no permitido");
+    // Limpieza de datos
+    const nombreLimpio = nombre?.trim();
+    const correoLimpio = correo?.trim().toLowerCase();
+
+    // Validaciones básicas
+    if (!nombreLimpio || !correoLimpio || !contrasenia || !tipo) {
+      throw new Error("Todos los campos son obligatorios");
     }
 
-    const hash = await bcrypt.hash(data.contrasenia, 10);
+    // Validar nombre
+    if (nombreLimpio.length < 3 || nombreLimpio.length > 100) {
+      throw new Error("El nombre debe tener entre 3 y 100 caracteres");
+    }
+
+    // Validar correo
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(correoLimpio)) {
+      throw new Error("El correo ingresado no es válido");
+    }
+
+    // Validar contraseña segura
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.#_-])[A-Za-z\d@$!%*?&.#_-]{8,}$/;
+
+    if (!passwordRegex.test(contrasenia)) {
+      throw new Error(
+        "La contraseña debe tener mínimo 8 caracteres, mayúsculas, minúsculas, número y símbolo",
+      );
+    }
+
+    // Verificar correo único
+    const usuarioExistente = await Usuario.buscarPorEmail(correoLimpio);
+    if (usuarioExistente) {
+      throw new Error("El correo ya está registrado");
+    }
+
+    // Verficar roles permitidos
+    const rolesPermitidos = ["estudiante", "profesor"];
+
+    if (!rolesPermitidos.includes(tipo)) {
+      throw new Error("El rol seleccionado no está permitido");
+    }
+
+    // Hash
+    const hash = await bcrypt.hash(contrasenia, 10);
 
     const id = await Usuario.crear({
-      nombre: data.nombre,
-      correo: data.correo,
+      nombre: nombreLimpio,
+      correo: correoLimpio,
       contrasenia: hash,
-      tipo: data.tipo,
+      tipo: tipo,
     });
 
     return id;
