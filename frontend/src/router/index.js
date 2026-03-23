05f1568ee72317ddfe55e3d32417edde5183f51e
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-23 03:22:38 -0600

    Separación rutas

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,33 +1,62 @@
-import { createRouter, createWebHistory } from "vue-router";
-import { useAuthStore } from "../store/auth";
+import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '../store/auth'
 
-import Login from "../views/Login.vue";
-import Dashboard from "../views/Dashboard.vue";
-import Reservaciones from "../views/Reservaciones.vue";
-import Espacios from "../views/Espacios.vue";
-import Registro from "../views/Registro.vue";
+import Login from '../views/Login.vue'
+import Dashboard from '../views/Dashboard.vue'
+import Reservaciones from '../views/Reservaciones.vue'
+import Espacios from '../views/Espacios.vue'
+import Registro from '../views/Registro.vue'
+import MainLayout from '../layouts/MainLayout.vue'
 
 const rutas = [
-  { path: "/", component: Login },
-  { path: "/registro", component: Registro },
-  { path: "/dashboard", component: Dashboard },
-  { path: "/reservaciones", component: Reservaciones },
-  { path: "/espacios", component: Espacios },
-];
+  { path: '/', component: Login },
+  { path: '/registro', component: Registro },
+  {
+    path: '/',
+    component: MainLayout,
+    children: [
+      { path: 'dashboard', component: Dashboard },
+      {
+        path: 'reservaciones',
+        component: Reservaciones,
+        meta: { roles: ['estudiante', 'profesor'] },
+      },
+      { path: 'espacios', component: Espacios, meta: { tipo: 'admin' } },
+    ],
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes: rutas,
-});
+})
 
-router.beforeEach((to, from) => {
-  const auth = useAuthStore();
+router.beforeEach((to) => {
+  const auth = useAuthStore()
 
-  if (to.path !== "/" && to.path !== "/registro" && !auth.token) {
-    return "/";
+  const rutasPublicas = ['/', '/registro']
+  const esPublica = rutasPublicas.includes(to.path)
+
+  // Usuaario no autenticado
+  if (!esPublica && !auth.token) {
+    return '/'
   }
 
-  return true;
-});
+  // Ya logueado => evitar volver a login
+  if (esPublica && auth.token) {
+    return '/dashboard'
+  }
 
-export default router;
+  // Validación por roles
+  if (to.meta.roles) {
+    const tipoUsuario = auth.usuario?.tipo
+
+    if (!to.meta.roles.includes(tipoUsuario)) {
+      return '/dashboard'
+    }
+  }
+
+  return true
+})
+
+export default router
