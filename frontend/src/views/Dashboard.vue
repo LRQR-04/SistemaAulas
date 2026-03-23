58860a83ec635266e00bc1d09f1b12d230bc576d
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-23 03:22:26 -0600

    Simplificación Dashboard

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,12 +1,7 @@
-<script setup>
-import MainLayout from "../layouts/MainLayout.vue";
-</script>
+<script setup></script>
 
 <template>
-  <MainLayout>
+  <div>
     <h1>Dashboard</h1>
-    <router-link to="/espacios">Ver aulas</router-link>
-    <br />
-    <router-link to="/reservaciones">Mis reservas</router-link>
-  </MainLayout>
+  </div>
 </template>
